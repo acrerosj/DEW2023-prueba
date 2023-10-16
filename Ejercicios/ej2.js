2f4b46bf57420505f65cfc05687b98bbc86448d1
Andrés Crespo <acrerosj@canariaseducacion.es>
2023-10-16 18:32:10 +0100

    Ejer 2,3,4 done

---
+++ Ejercicios/ej2.js
@@ -0,0 +1,22 @@
+// Media de longitud de palabras
+
+function avgLengthWords(text) {
+    //text = text.replace(/[.,:;]/g,'');
+    const signPoints = '.,:;';
+//    signPoints.split('').forEach(s => text = text.replace(s, ''));
+    for (let i=0; i<signPoints.length; i++) {
+        text = text.replace(signPoints[i], '');
+    }
+    const words = text.split(" ");
+    let lengths = words.map(w => w.length);
+    return lengths.reduce((a,l) => a+l, 0) / words.length;
+}
+
+let text = ""; //0
+console.log(text, "->", avgLengthWords(text));
+
+text = "Este texto tiene palabras más palabras y signos"; //5
+console.log(text, "->", avgLengthWords(text));
+
+text = "Este texto tiene: palabras, más palabras y signos."; //5
+console.log(text, "->", avgLengthWords(text));
